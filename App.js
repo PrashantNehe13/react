ddb4d856ee7705f97f6e5054763bdab11ae9a738
Prashant Nehe <prashantnehe@ip-192-168-11-3.ap-south-1.compute.internal>
2025-03-26 16:15:25 +0530

    Child ID name changed

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id: "child1"}, [
+    React.createElement("div", {id: "child0"}, [
         React.createElement("h1", {id: "heading1"}, "I am heading 1"), 
         React.createElement("h2", {id: "heading2"}, "I am heading 2")
     ]),
-    React.createElement("div", {id: "child2"}, [
+    React.createElement("div", {id: "child1"}, [
         React.createElement("h1", {id: "heading3"}, "I am heading 1"), 
         React.createElement("h2", {id: "heading4"}, "I am heading 2")
     ]) 
