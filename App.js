0897c198b301fdefc7aafeec10a981d0f69dea9a
Prashant Nehe <prashantnehe@ip-192-168-11-3.ap-south-1.compute.internal>
2025-03-26 16:05:38 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child1"}, [
+        React.createElement("h1", {id: "heading1"}, "I am heading 1"), 
+        React.createElement("h2", {id: "heading2"}, "I am heading 2")
+    ]),
+    React.createElement("div", {id: "child2"}, [
+        React.createElement("h1", {id: "heading3"}, "I am heading 1"), 
+        React.createElement("h2", {id: "heading4"}, "I am heading 2")
+    ]) 
+])
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
